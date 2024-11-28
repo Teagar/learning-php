4eb59075b093f67ff191eb92f9c5449a5103e7e0
teagar <cyanofc@gmail.com>
2024-11-28 04:36:19 -0300

    09

---
+++ 09-funcoes-de-arrays.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * is_array($array)
+ * in_array($value, $array)
+ * array_keys($array)
+ * array_values($array)
+ * array_merge($array1, $array2)
+ * array_pop($array)
+ * array_shift($array)
+ * array_unshift($array, "value")
+ * array_push($array, "value", "value")
+ * array_combine($keys, $values)
+ * array_sum()
+ * explode("/", "20/01/2001")
+ * implode("-", $arr)
+ */
+
+$names = ["Thiago", "Joao", "Luccas"];
+$forenames = ["Cerqueira", "Lima", "Azevedo"];
+
+// make the tests below
