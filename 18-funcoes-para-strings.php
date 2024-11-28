ee08ade3201572305544bace0a731fa8228ad3b9
teagar <cyanofc@gmail.com>
2024-11-28 05:53:33 -0300

    strings functions

---
+++ 18-funcoes-para-strings.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * strtoupper($value)
+ * strtolower($value)
+ * substr($value, $index, $size)
+ * str_pad($value, $size, $char, param)
+ * str_repeat($value, $times)
+ * strlen($value)
+ * str_replace($search, $replace, $value)
+ * strpos($value, $search)
+ */
