c765c7a05669b3306d09c06e2b93a094c22c6284
teagar <cyanofc@gmail.com>
2024-11-28 06:39:00 -0300

    GET & POST

---
+++ 22-formularios/index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="dados.php" method="POST">
+Nome: <input type="text" name="name"><br>
+Email: <input type="email" name="email"><br>
+<button type="submit"> Send </button>
+</form>
+<a href="dados.php?userpassword=1234&userID=189"> Send data </a>
+
+</body>
+</html>
