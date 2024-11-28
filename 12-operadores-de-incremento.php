5106f2a5d77c3739827ef5c739ccf091a5accc6f
teagar <cyanofc@gmail.com>
2024-11-28 05:00:18 -0300

    Increment and decrement

---
+++ 12-operadores-de-incremento.php
@@ -0,0 +1,18 @@
+<?php
+
+$value = 20;
+
+// Pre-increment
+echo ++$value;
+// Pos-increment
+echo $value++;
+
+echo $value;
+
+
+// Pre-decrement
+echo --$value;
+// Pos-decrement
+echo $value--;
+
+echo $value;
