cdb3556454ad0a9700831de6e238e6786156e4d1
teagar <cyanofc@gmail.com>
2024-11-28 09:56:39 -0300

    Validate filters(working)

--- 22-formularios/index.php
+++ 22-formularios/index.php
@@ -1,12 +1,22 @@
 <html>
 <body>
 
-<form action="dados.php" method="POST">
+<?php
+if (isset($_POST['send-form'])) {
+  $errors = [];
+  $yearsOld = $_POST['yearsOld'];
+  if($yearsOld = filter_input(INPUT_POST, 'yearsOld', FILTER_VALIDATE_INT)) {
+    echo "Int";
+  }
+}
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
 Nome: <input type="text" name="name"><br>
 Email: <input type="email" name="email"><br>
-<button type="submit"> Send </button>
+Years: <input type="number" name="yearsOld">
+<button type="submit" name="send-form"> Send </button>
 </form>
-<a href="dados.php?userpassword=1234&userID=189"> Send data </a>
 
 </body>
 </html>
