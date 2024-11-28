b3a4d85b1b9577e502fdca76226c1c674b12a85c
teagar <cyanofc@gmail.com>
2024-11-28 05:44:40 -0300

    For & foreach

---
+++ 17-for-foreach.php
@@ -0,0 +1,11 @@
+<?php
+
+for ($counter = 0; $counter <= 10; $counter++) {
+  echo "Counter = $counter<br>";
+}
+
+$colors = ["blue", "red", "green"];
+
+foreach ($colors as $key => $value) {
+  echo $value."<br>";
+}
