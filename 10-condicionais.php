1b5823b40b66be6eab2d598f9f66a94681183e5f
teagar <cyanofc@gmail.com>
2024-11-28 04:46:07 -0300

    conditionals

---
+++ 10-condicionais.php
@@ -0,0 +1,17 @@
+<?php
+
+$number= 10;
+
+if ($number == 10) {
+  echo "Is equals ten";
+} elseif ($number <= 9) {
+  echo "Is above ten";
+} else {
+  echo "Nah";
+}
+
+$media = 7;
+
+echo "<hr>";
+
+echo ($media >= 7) ? "Congrat" : "Bruh";
