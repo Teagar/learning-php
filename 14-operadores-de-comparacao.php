f1ab8b1efe6f712a9fd134ee014a4dd32b48816f
teagar <cyanofc@gmail.com>
2024-11-28 05:09:50 -0300

    comparation

---
+++ 14-operadores-de-comparacao.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * ==
+ * !=
+ * ===
+ * !==
+ * <>
+ * <
+ * >
+ * <=
+ * >=
+ * <=>
+ */
+
+var_dump(20 <=> 30); // if the left side is minus will return -1; if the right returns 1; if equals returns 0
