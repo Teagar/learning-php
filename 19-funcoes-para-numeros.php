f129c67cafaeaebf6c0808a2cbb7b7b10b9ac4ae
teagar <cyanofc@gmail.com>
2024-11-28 05:56:59 -0300

    number functions

---
+++ 19-funcoes-para-numeros.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * number_format($number, $decimal, $decimalSeparator, $milenalSeparator)
+ * round($number)
+ * ceil($number)
+ * floor($number)
+ * rand($initialValue, $finalValue)
+ */
