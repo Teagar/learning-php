15cd06e8e86d1b34ed1fbe8a630a68d22e9443b2
teagar <cyanofc@gmail.com>
2024-11-29 12:16:10 -0300

    Validate filters

--- 22-formularios/index.php
+++ 22-formularios/index.php
@@ -2,19 +2,42 @@
 <body>
 
 <?php
-if (isset($_POST['send-form'])) {
+/* Validations
+ * Functions (filter_input - filter_var)
+ * filter_input(INPUT_METHOD, $name, FILTER_VALIDATE_TYPE)
+ * FILTER_VALIDATE_INT
+ * FILTER_VALIDATE_EMAIL
+ * FILTER_VALIDATE_FLOAT
+ * FILTER_VALIDATE_IP
+ * FILTER_VALIDATE_URL
+ */
+?>
+
+<?php
+// Verify if the button has clicked
+$sendFormExists = isset($_POST['send-form']);
+if ($sendFormExists) {
   $errors = [];
   $yearsOld = $_POST['yearsOld'];
-  if($yearsOld = filter_input(INPUT_POST, 'yearsOld', FILTER_VALIDATE_INT)) {
-    echo "Int";
+  if(!$yearsOld = filter_input(INPUT_POST, 'yearsOld', FILTER_VALIDATE_INT)) {
+    $errors[] = "Age needs to be a int";
+  }
+  if(!empty($errors)) {
+    foreach ($errors as $error){
+      echo "<li>$error</li>";
+    }
+  } else {
+    echo "All is been okay";
   }
 }
 ?>
 
 <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
-Nome: <input type="text" name="name"><br>
+Age: <input type="text" name="yearsOld"><br>
 Email: <input type="email" name="email"><br>
-Years: <input type="number" name="yearsOld">
+Weigth: <input type="text" name="Weigth"><br>
+IP: <input type="text" name="ip"><br>
+URL: <input type="text" name="url"><br>
 <button type="submit" name="send-form"> Send </button>
 </form>
 
